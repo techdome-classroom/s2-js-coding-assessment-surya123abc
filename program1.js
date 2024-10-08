fdfc1c8c1ca03b83a12b933ce5f5916de5579e4f
surya123abc <suryansees@gmail.com>
2024-10-08 13:51:58 +0530

    Update program1.js

--- program1.js
+++ program1.js
@@ -3,6 +3,27 @@
  * @return {boolean}
  */
 var isValid = function(s) {
+        let stack = [];
+    let map = {
+        '(': ')',
+        '[': ']',
+        '{': '}'
+    };
+
+    for (let i = 0; i < s.length; i++) {
+        let char = s[i];
+        if (map[char]) {
+            stack.push(char);
+        } else {
+            let last = stack.pop();
+            if (char !== map[last]) {
+                return false;
+            }
+        }
+    }
+
+    return stack.length === 0;
+
     
 };
 
